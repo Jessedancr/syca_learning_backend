22d71449572fde5269d4f874743232a763067fce
Ikemefuna Jesse <jesseikemefuna@gmail.com>
2024-07-23 16:18:04 +0100

    Fixed histories bug

--- converter/index.js
+++ converter/index.js
@@ -13,7 +13,7 @@ const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 client.connect().then(() => console.log("Connected to Mongo DB"));
 
-// Passing DB and collections name to variables
+// Passing DB and history collections name to variables
 const db = client.db("users");
 const historyCollection = db.collection("history");
 
@@ -40,7 +40,6 @@ app.use(
 	}),
 );
 
-let usersList = [];
 const options = ["°C to °F", "M to Cm", "KG to lbs", "inch to Cm"];
 let conversion;
 
@@ -49,16 +48,65 @@ app.get("/", (req, res) => {
 	res.send("H O M E P A G E");
 });
 
-app.get("/convert", (req, res) => {
-	res.render("convert", { options, conversion });
+// SIGN UP ENDPOINTS
+app.get("/signup", async (req, res) => {
+	console.log("/signup by GET has been hit");
+	res.render("signup");
+});
+app.post("/signup", async (req, res) => {
+	console.log("/signup by POST has been hit");
+	const newUser = req.body;
+	try {
+		if (newUser.username && newUser.password) {
+			// Add new user to DB
+			const usersCollection = await req.db.collection("converter_users");
+			const insertUser = await usersCollection.insertOne(newUser);
+			console.log("New User", insertUser);
+			res.render("on_signup");
+		}
+	} catch (error) {
+		console.log(error);
+	}
 });
 
+// LOGIN ENDPOINTS
+app.get("/login", async (req, res) => {
+	res.render("login");
+});
+app.post("/login", async (req, res) => {
+	try {
+		const { username, password } = req.body; // Destructuring req body extracting these props from it
+		const collection = await req.db.collection("converter_users");
+		const user = await collection.findOne({ username, password });
+		if (user) {
+			req.session.user = { username: user.username, id: user._id }; // Adding the user prop to the session object and the id from mongo DB
+			console.log(req.session); // To confirm the user object has been inserted into the session object
+			console.log("Logged in as", user);
+			res.render("convert", { options, conversion });
+		} else {
+			res.send("INVALID USER");
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+// CONVERT ENDPOINTS
+app.get("/convert", (req, res) => {
+	if (req.session.user) {
+		res.render("convert", { options, conversion });
+	} else {
+		res.redirect("/login");
+	}
+});
 app.post("/convert", (req, res) => {
-	console.log("Request body: ", req.body);
-	console.log("Properties on Req body ", Object.keys(req.body));
+	if (!req.session.user) {
+		res.redirect("/login");
+	}
 
 	const conversionType = req.body.conversionType;
 	const numberInput = parseFloat(req.body.numberInput);
+	const userID = req.session.user.id;
 
 	if (conversionType === "°C to °F") {
 		conversion = (numberInput * 9) / 5 + 32;
@@ -76,59 +124,22 @@ app.post("/convert", (req, res) => {
 
 	// STORE CONVERSION HISTORY IN DB
 	historyCollection.insertOne({
+		userID,
 		conversionType,
 		numberInput,
 		conversion,
+		timeStamp: new Date(),
 	});
 });
 
+// HISTORY ENDPOINT
 app.get("/history", async (req, res) => {
-	const conversionHistory = await historyCollection.find().toArray();
+	const userID = req.session.user.id;
+	const conversionHistory = await historyCollection.find({ userID }).toArray(); // Finding by user ID
 	console.log("From /history:", conversionHistory);
 	res.render("history", { history: conversionHistory });
 });
 
-app.get("/signup", async (req, res) => {
-	console.log("/signup by GET has been hit");
-	res.render("signup");
-});
-app.post("/signup", async (req, res) => {
-	console.log("/signup by POST has been hit");
-	const newUser = req.body;
-	try {
-		if (newUser.username && newUser.password) {
-			usersList.push(newUser);
-
-			// Add new user to DB
-			const usersCollection = await req.db.collection("converter_users");
-			const insertUser = await usersCollection.insertOne(newUser);
-			console.log("New User", insertUser);
-			res.render("on_signup");
-		}
-	} catch (error) {
-		console.log(error);
-	}
-});
-
-app.get("/login", async (req, res) => {
-	res.render("login");
-});
-
-app.post("/login", async (req, res) => {
-	const { username, password } = req.body;
-	try {
-		const collection = await req.db.collection("converter_users");
-		const user = await collection.findOne({ username, password });
-		if (user) {
-			console.log("Logged in as", user);
-			res.render("convert", { options, conversion });
-		} else {
-			res.send("INVALID USER");
-		}
-	} catch (error) {
-		console.log(error);
-	}
-});
 app.listen(port, (req, res) => {
 	console.log(`Server started on http://localhost:${port}`);
 });
